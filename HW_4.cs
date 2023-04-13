25a24383623d9c40bafdeec2f75dff446430845d
OlgaDobrydina <125290861+OlgaDobrydina@users.noreply.github.com>
2023-04-13 21:52:28 +0500

    Add files via upload

---
+++ HW_4.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число ");
+string input = Console.ReadLine();
+int number1 = int.Parse(input);
+Console.WriteLine("Ведите второе число ");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+Console.WriteLine("Ведите третье число ");
+string input3 = Console.ReadLine();
+int number3 = int.Parse(input3);
+int max = number1;
+
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine("Максимальное число ");
+Console.WriteLine(max);
---
+++ HW_6.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+if (number % 2 == 0) Console.WriteLine("Число четное ");
+else Console.WriteLine("Число нечетное ");
